ce4c4947d6d19249cd9c6841d825f20c5c496193
Javier Iglesias García <Javi.igl.gar@gmail.com>
2020-12-14 12:35:19 +0100

    feat(eslint): tweak rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const typescriptRules = require('./config/typescript.eslint.json');
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:import/errors',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  reportUnusedDisableDirectives: true,
+  rules: {
+    ...typescriptRules
+  }
+};
